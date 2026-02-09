841aba8d81038e0be188c666936d644066453b51
Nurbolllll <ntalgatov2007nur@gmail.com>
2026-02-09 09:09:35 +0500

    lab1 finished

---
+++ lab1/main1.java
@@ -0,0 +1,16 @@
+package lab1;
+
+public class main1 {
+    public static void main(String[] args) {
+        // Данные студента
+        String fio = "Талгатов Нурбол";
+        String group = "ИС 24-22";
+
+        System.out.println("========================================");
+        System.out.println("   ИНФОРМАЦИЯ О СТУДЕНТЕ");
+        System.out.println("========================================");
+        System.out.println("ФИО:         " + fio);
+        System.out.println("Группа:      " + group);
+        System.out.println("========================================");
+    }
+}
